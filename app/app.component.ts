71970973a198373aaac0d84de3d6e538bee43d3d
Michal Dymel <michal@dymel.pl>
2016-11-02 23:22:41 +0100

    i18n implementation

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,21 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  template: `
+  	<h1>My First Angular App</h1>
+  	<h2 i18n="header">Login form</h2>
+	<div>
+	  <span i18n>Email</span>
+	  <input type="text" placeholder="Your email" i18n-placeholder>
+	</div>
+	<div>
+	  <span i18n>Password</span>
+	  <input type="password" placeholder="Password" i18n-placeholder>
+	</div>
+
+	<hr>
+
+	<div i18n>If you want to search something, go to <a href="http://google.com">Google</a></div>
+  	`
 })
 export class AppComponent { }
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
+import { getTranslationProviders } from './i18n-providers';
+
+getTranslationProviders().then(providers => {
+  const options = { providers };
+  platformBrowserDynamic().bootstrapModule(AppModule, options);
+});
